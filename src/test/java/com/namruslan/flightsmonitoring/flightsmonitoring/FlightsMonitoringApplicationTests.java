38d9c94e1cdb9bdcfbd84a9453b620dd010d3f30
NamRuslan <rus.nam.96@bk.ru>
2021-03-27 00:31:41 +0300

    Created Project

---
+++ src/test/java/com/namruslan/flightsmonitoring/flightsmonitoring/FlightsMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namruslan.flightsmonitoring.flightsmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
